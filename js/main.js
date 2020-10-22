6007114c5463099fd6226d314b8a90813130035e
Hossik <hossik1983@gmail.com>
2020-10-22 18:36:59 +0300

    rep

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const fetchUsers = async () => 
   await(await fetch('http://localhost:9000/getusers')).json();
-
 fetchUsers().then(data => {
     userList = document.querySelector('#users');
     data.forEach(user => {
