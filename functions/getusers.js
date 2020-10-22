785baa64adda5ff0f4426082b4b3ff0cfbdf648e
Hossik <hossik1983@gmail.com>
2020-10-22 18:41:40 +0300

    deployr

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 
-exports.handler = function(event, context, callback){
-  const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
-
+exports.handler = function(event, contex, callback) {
+  const API_URL = 'https://api.github.com/users';
+  const API_CLIENT_ID = '2fcf9ff37aaa320bc327';
+  const API_CLIENT_SECRET = 'af1a3c5176e54838f403d81493b70f0544119295';
   const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
 
   // Send user response
@@ -11,23 +12,23 @@ exports.handler = function(event, context, callback){
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers':
-          'Origin, X-Requested-With, Content-Type, Accept'
+        'Access-Control-Allow-Headers': 'Origin, X-Requeted-With, Content-Type, Accept'
       },
       body: JSON.stringify(body)
     });
   }
 
-  // Perform API call
+  // Perform API callback
   const getUsers = () => {
     axios.get(URL)
-      .then(res => send(res.data))
-      .catch(err => send(err));
+    .then(res => send(res.data))
+    .catch(err => send(err))
   }
 
-  // Make sure method is GET
+  //Make sure method is GET
+
   if(event.httpMethod == 'GET') {
-    // Run
+    //RUN
     getUsers();
   }
 }
