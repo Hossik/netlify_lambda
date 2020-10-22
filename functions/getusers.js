22bcd2d9df6b8240396a6733e047225844a2558a
Hossik <hossik1983@gmail.com>
2020-10-22 19:21:13 +0300

    redeplo

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 
 exports.handler = function(event, contex, callback) {
-  const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env;
-
+  const API_URL = 'https://api.github.com/users';
+  const API_CLIENT_ID = '2fcf9ff37aaa320bc327';
+  const API_CLIENT_SECRET = 'af1a3c5176e54838f403d81493b70f0544119295';
   const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
 
   // Send user response
